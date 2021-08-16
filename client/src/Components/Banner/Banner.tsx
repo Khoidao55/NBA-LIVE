095473881435a8dcbcc14e362eeef88d0e8561e2
Khoi Dao <khoidao55@gmail.com>
2021-08-16 13:40:54 -0700

    created a list for the menu bar

--- client/src/Components/Banner/Banner.tsx
+++ client/src/Components/Banner/Banner.tsx
@@ -11,17 +11,35 @@ const Banner: React.FC<Props> = () => {
   const [click, setClick] = useState<boolean>(false);
 
   const handleClick = () => setClick(!click);
-  console.log(click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <>
-    <nav className="navbar">
-      <div className="navbar-container">
-        <Link to="/" className="navbar-logo">
-          <img src={NBALOGO} className="logo"/>
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <Link to='/' className='navbar-logo'>
+          <img src={NBALOGO} className='logo'/>
         </Link>
-        <div className="menu-icon" onClick={handleClick}>
+        <div className='menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu disabled'}>
+          <li className='nav-item'>
+            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+              Player Stats
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+              News
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
     </>
