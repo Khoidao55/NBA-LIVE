66172d7238f236ca6b42af12b40b82a868c554cc
Khoi Dao <khoidao55@gmail.com>
2021-08-16 13:30:23 -0700

    added images and declaration file for TS

--- client/src/Components/Banner/Banner.tsx
+++ client/src/Components/Banner/Banner.tsx
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import NBALOGO from '../../../images/nba-logo.png';
+import './Banner.css';
 
 interface Props {
 
 }
 
-export const Banner: React.FC<Props> = () => {
+const Banner: React.FC<Props> = () => {
+  const [click, setClick] = useState<boolean>(false);
+
   return (
     <>
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-          NBALOGO
+          <img src={NBALOGO} className="logo"/>
         </Link>
+        <div className="menu-icon">
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
       </div>
     </nav>
     </>
   );
 }
+
+export default Banner;
