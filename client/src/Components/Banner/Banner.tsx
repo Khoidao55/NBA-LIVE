b51783ea677adca8a1207d8373f1b02b52b84721
Khoi Dao <khoidao55@gmail.com>
2021-08-17 09:14:23 -0700

    added CSS to navbar

--- client/src/Components/Banner/Banner.tsx
+++ client/src/Components/Banner/Banner.tsx
@@ -29,7 +29,7 @@ const Banner: React.FC<Props> = () => {
     <nav className='navbar'>
       <div className='navbar-container'>
         <Link to='/' className='navbar-logo'>
-          <img src={NBALOGO} className='logo'/>
+          <img src={NBALOGO} className='nba-logo'/>
         </Link>
         <div className='menu-icon' onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
