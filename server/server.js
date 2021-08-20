d660048e7ebba0213311a7b54b8854a953ad548b
Khoi Dao <khoidao55@gmail.com>
2021-08-20 13:30:10 -0700

    added redis and made a controller file for routes

--- server/server.js
+++ server/server.js
@@ -2,8 +2,18 @@ const express = require('express');
 const axios = require('axios');
 const { liveOptions } = require('../API.js');
 const bodyParser = require('body-parser');
+const redis = require('redis');
+
+const EXPRESS_PORT = 3000;
+const REDIS_PORT = 6379;
 
 const app = express();
+const client = redis.createClient(REDIS_PORT);
+
+
+client.on('connect', function() {
+  console.log('Connected to Redis!');
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,7 +28,7 @@ app.get('/getLiveData' , async (req, res) => {
   res.send(liveData.data.api);
 });
 
-app.listen(3000, () => {
+app.listen(EXPRESS_PORT, () => {
   console.log("Listening on 3000, the best server");
 })
 
